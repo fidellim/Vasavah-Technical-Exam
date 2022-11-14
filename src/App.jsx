30d2dd9406fb3182cfea53d8a476bfe87e5e5fe9
fidellim <lim.fidel@gmail.com>
2022-11-14 15:17:48 +0400

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { Box } from '@mui/material'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <Box>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+            </Routes>
+        </Box>
+    )
 }
 
 export default App
