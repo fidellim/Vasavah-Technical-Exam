5bb4e997e94dcfcb67f77bbf80db1deff0dc0831
fidellim <lim.fidel@gmail.com>
2022-11-15 04:59:02 +0400

    pass firebasePath prop to child

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -2,27 +2,29 @@ import { Typography, Box } from '@mui/material'
 import React from 'react'
 import Log from './Log'
 
-const Logs = ({ logs }) => {
+const Logs = ({ logs, firebasePath }) => {
     return (
         <Box>
-            <Typography
-                variant="h2"
-                sx={{
-                    fontSize: {
-                        xs: '1.5rem',
-                        sm: '2rem',
-                    },
-                    marginBottom: '.75rem',
-                    color: 'black',
-                }}
-            >
-                {`Log${logs.length > 1 && 's'}`}
-            </Typography>
+            {logs.length !== 0 && (
+                <Typography
+                    variant="h2"
+                    sx={{
+                        fontSize: {
+                            xs: '1.5rem',
+                            sm: '2rem',
+                        },
+                        marginBottom: '.75rem',
+                        color: 'black',
+                    }}
+                >
+                    {`Log${logs.length > 1 ? 's' : ''}`}
+                </Typography>
+            )}
             <Box
                 sx={{ display: 'flex', flexDirection: 'column', gap: '.5rem' }}
             >
                 {logs.map((log) => (
-                    <Log key={log.id} log={log} />
+                    <Log key={log.id} log={log} firebasePath={firebasePath} />
                 ))}
             </Box>
         </Box>
